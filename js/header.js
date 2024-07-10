d78db1b22ae10a545c2e45cf78c288114d86a846
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-07-10 12:19:30 +0300

    header

--- js/header.js
+++ js/header.js
@@ -1,15 +1,15 @@
-const header = document.createElement('header');
-header.innerHTML = `
-    <header>
-        <nav>
-            <a href="../">Home</a>
-            <a href="../products/">Products</a>
-            <a href="../about-us/">About us</a>
-            <a href="../contact/">Contact</a>
-            <a href="../pokemon/">Pokemon</a>
-            <a href="../sportas/">Sportas</a>
-        </nav>
-    </header>
-`;
+// const header = document.createElement('header');
+// header.innerHTML = `
+//     <header>
+//         <nav>
+//             <a href="../">Home</a>
+//             <a href="../products/">Products</a>
+//             <a href="../about-us/">About us</a>
+//             <a href="../contact/">Contact</a>
+//             <a href="../pokemon/">Pokemon</a>
+//             <a href="../sportas/">Sportas</a>
+//         </nav>
+//     </header>
+// `;
 
-document.body.prepend(header);
+// document.body.prepend(header);
